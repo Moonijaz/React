7b412af909fa48f98b00fd9f8a9cc0331c070095
Moon ijaz <moonijaz006@gmail.com>
2023-03-01 20:19:05 +0500

    Add header component

---
+++ my-app/src/Components/Header.js
@@ -0,0 +1,23 @@
+
+import './Header.css';
+
+function Header() {
+  return (
+  <>
+  <header>
+    <h1>Resume</h1>
+  </header>
+  <section>
+  <div className="head">
+        <h1>Mamoona Ijaz </h1>
+        <span>Street Address</span>
+        <span>Zipe Code</span>
+        <span>Phone Number</span>
+        <span>Email</span>
+      </div>
+    </section>
+  </>
+  )
+}
+
+export default Header;
