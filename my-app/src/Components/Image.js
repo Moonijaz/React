12e7c36a3d495bab0b1e162901e2cb21bde6fd6e
Moon ijaz <moonijaz006@gmail.com>
2023-03-09 18:10:05 +0500

    Add image

---
+++ my-app/src/Components/Image.js
@@ -0,0 +1,17 @@
+
+
+import './Image.css';
+
+function Image() {
+  return (
+  <>
+  <div className="Profile-image">
+  <img
+      src="./profile.jfif"
+      alt="Profile image"/>
+      </div>
+  </>
+  )
+}
+
+export default Image;
