dcf3bb8875e6a77584365f76c8549b1e7a3ad1db
Moon ijaz <moonijaz006@gmail.com>
2023-03-09 21:26:27 +0500

    Add header container js

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,20 +1,18 @@
-
-import './Header.css';
+import "./Header.css";
 
 function Header() {
   return (
-  <>
-  <section>
-  <div className="head">
-        <h1>Mamoona Ijaz </h1>
-        <span>Street Address</span>
-        <span>Zipe Code</span>
-        <span>Phone Number</span>
-        <span>Email</span>
-      </div>
-    </section>
-  </>
-  )
+    <>
+      <section>
+        <div className="head">
+          <h1>Mamoona Ijaz </h1>
+          <span>Street Address</span>
+          <span>Zipe Code</span>
+          <span>Phone Number</span>
+          <span>Email</span>
+        </div>
+      </section>
+    </>
+  );
 }
-
 export default Header;
